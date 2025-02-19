7cd927afc197c14bf400eefcd1f681b9cda4e0ff
Karen Monserrat <karen1.fresita02@gmail.com>
2025-02-18 22:11:20 -0600

    Primer commit

---
+++ Tipos de dato/tipos-de-dato.js
@@ -0,0 +1,42 @@
+/*
+    TIPOS DE DATOS EN JS
+*/
+
+// Tipo: Number 
+var numero = 42
+var numero2 = -666
+var numero3 = 0
+var 80 = 80
+var 16 = 16
+
+// Tipo: String
+var letranumero = " Veinticinco "
+var bienvenida = "Bienvenido"
+var despedida = "Adios"
+
+//Tipo: Boolean
+var aprobo = true //Verdadero
+var reprobo = false //Falso
+
+//Tipo: Symbol
+var simbolo = Symbol("id")
+
+//Tipo: BigInt 
+var pi = 3.141592653589n
+
+//Tipo: Null
+var vacio = null
+
+//Tipo: Undefined
+var sinDefinir
+
+var comilla = " . "
+
+
+
+console.log(numero)
+console.log(typeof numero)
+console.log(numero2)
+console.log(typeof numero2)
+console.log(16)
+console.log(typeof 16)
